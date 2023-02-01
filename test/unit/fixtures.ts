0c99f93cc6ae53f9f7c3f3e7f94dada195fc691b
Player Nguyên <daudaua3782@gmail.com>
2023-02-01 10:48:41 +0700

    add(test): clean up file system after tested

--- test/unit/fixtures.ts
+++ test/unit/fixtures.ts
@@ -1,4 +1,6 @@
+import fse from "fs-extra";
 import chalk from "chalk";
+import { getUserDataPath } from "../../electron/launcher/file";
 
 /**
  * Global before
@@ -10,9 +12,19 @@ before(() => {
       `${chalk.gray(`[-]`)} process.env.NODE_ENV: ${process.env.NODE_ENV}`
     )
   );
+  console.log(
+    chalk.green(`${chalk.gray(`[-]`)} userData: ${getUserDataPath()}`)
+  );
 });
 
 /**
  * Global after
  */
-after(() => {});
+after(() => {
+  // Cleaning up userData (.test_output) directory after used
+  console.log(
+    chalk.green(`${chalk.gray(`[-]`)} Cleaning up userData directory...`)
+  );
+
+  fse.rmdirSync(getUserDataPath());
+});
